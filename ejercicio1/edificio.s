8f5b0810c86747265c8f7a9773c7dbbded760852
joacolongar <joaquin.ortiz.longar@mi.unc.edu.ar>
2025-05-31 20:05:46 -0300

    cree edificio.s
    es una funcion que basicamente hace rectangulos, en el app.s nosotros cambiamos los valores de ancho largo y sus (x,y) para crear el edificio que queramos. tambien estan los colores

---
+++ ejercicio1/edificio.s
@@ -0,0 +1,32 @@
+.global edificio
+
+edificio:
+	
+	mov x10, x3          // alto
+	mov x11, x2          // ancho
+	mov x12, x1          // y inicial
+	mov x13, x0          // x inicial
+	mov x14, x14         // color (ya está cargado en app.s)
+	mov x18, 640         // el maximo de la pantalla
+
+rectloopy:
+	mov x15, x11         // contador x
+	mov x16, x13         // x actual
+	mov x17, x12         // y actual
+
+rectloopx:
+	mul x0, x17, x18     // offset = y * (maximo de pantalla)
+	add x0, x0, x16      // + x
+	lsl x0, x0, 2        // * 4 bytes por píxel
+	add x0, x20, x0      // dirección final
+	stur x14, [x0]        // pintar píxel
+
+	add x16, x16, 1
+	sub x15, x15, 1
+	cbnz x15, rectloopx
+
+	add x12, x12, 1
+	sub x10, x10, 1
+	cbnz x10, rectloopy
+
+	ret
