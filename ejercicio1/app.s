70e7d99b9373ab457895ef9d17418fa7a01fa5a0
joacolongar <joaquin.ortiz.longar@mi.unc.edu.ar>
2025-06-03 20:52:29 -0300

    implemente pajarito.s
    hicimos un archivo el cual nos dibuja la silueta de un pajaro, en donde nosotros modificamos parametros en app.s para ubicar los pajaros en la pantalla

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -223,33 +223,18 @@ cd12:
 	mov x4,1
 	bl letras
 
+	mov x1, 36
+	mov x0, 50
+	bl pajarito
 
-
-
-
-
-
+	mov x1 , 50
+    mov x0, 68
+	bl pajarito
 	
+	mov x1 , 64
+	mov x0, 50
+	bl pajarito
 
-	
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
-
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
-
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
-
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
-
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
-
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
 
 	//---------------------------------------------------------------
 	// Infinite Loop
